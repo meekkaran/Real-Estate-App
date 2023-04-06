0d4b7f3444257f84597e7ee80a0538c86da6f1b5
Meek Karan <meekkaran@gmail.com>
2023-04-06 09:04:50 +0300

    working on home page

---
+++ frontend/src/components/ListingForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  ListingForm = () => {
+    return (
+        <div>ListingForm</div>
+    ); 
+}
+
+export default ListingForm
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Helmet } from "react-helmet";
+import ListingForm from '../components/ListingForm';
+import Listings from '../components/Listings';
+import Pagination from '../components/Pagination';
 
-const  home = () => {
+
+const  Home = () => {
+    const [listings, setListings] = useState([]);
+    const [currentPage, setcurrentPage] = useState(1);
+    const [listingsPerPage, setlistingsPerPage] = useState(3);
+    const [active, setActive] = useState(1);
+
+    const indexOfLastListing = currentPage * listingsPerPage;
+    const indexOfFirstListing = indexOfLastListing - listingsPerPage;
+    const currentListings = listings.slice(indexOfFirstListing, indexOfLastListing);
+
+    const visitPage = (page) => {
+        setcurrentPage(page);
+        setActive(page);
+    };
+
+    const previous_number = () => {
+        if (currentPage !== 1){
+            setcurrentPage(currentPage-1);
+            setActive(currentPage-1)
+        }
+    };
+
+    const next_number = () => {
+        if (currentPage !== Math.ceil(listings.length/3)){
+            setcurrentPage(currentPage+1);
+            setActive(currentPage+1)
+        }
+    };
+
+
     return(
-        <div>home</div>
+        <main className='home'> 
+            <Helmet>
+                <title>Realest Estate</title>
+            </Helmet>
+        </main>
     ); 
-}
+};
 
-export default home
+export default Home
